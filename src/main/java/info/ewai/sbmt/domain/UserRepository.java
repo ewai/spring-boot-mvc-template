d0d534e20be098d53aaa9cf0b03cedfa2984f1c3
ewai <ewai.info@gmail.com>
2017-08-08 18:45:19 +0900

    gitignore

--- src/main/java/info/ewai/sbmt/domain/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package info.ewai.sbmt.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUsername(String username);
-}
